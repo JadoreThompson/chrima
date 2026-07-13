25d7e9a1d5a82193c251fd6e52bb19694c421177
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-07-13 16:47:50 +0100

    refactor: ChrimaPayment contract to USDT-only with TestUSDT for dev

--- src/resources/contract/contracts/ChrimaPayment.sol
+++ src/resources/contract/contracts/ChrimaPayment.sol
@@ -6,17 +6,12 @@ interface IERC20 {
     function decimals() external view returns (uint8);
 }
 
-interface IExchangeRateProvider {
-    function getRate(string memory base, string memory quote) external view returns (uint256 rate, uint256 updatedAt);
-}
-
 contract ChrimaPayment {
     address public owner;
-    IExchangeRateProvider public exchangeRateProvider;
-    bool public contractDisabled;
+    address public usdtToken;
 
-    mapping(address => string) public tokenSymbols;
     mapping(string => uint256) public priceIdToAmount;
+    mapping(string => address) public productIdToRecipient;
 
     event TransactionComplete(
         string product_id,
@@ -24,18 +19,7 @@ contract ChrimaPayment {
         string user_id,
         address indexed sender,
         address indexed recipient,
-        address token,
-        uint256 tokenAmount
-    );
-
-    event TransactionFailed(
-        string product_id,
-        string price_id,
-        string user_id,
-        address indexed sender,
-        address indexed recipient,
-        address token,
-        string reason
+        uint256 amount
     );
 
     modifier onlyOwner() {
@@ -43,18 +27,13 @@ contract ChrimaPayment {
         _;
     }
 
-    modifier whenEnabled() {
-        require(!contractDisabled, "Contract is disabled");
-        _;
-    }
-
-    constructor(address _exchangeRateProvider) {
+    constructor() {
         owner = msg.sender;
-        exchangeRateProvider = IExchangeRateProvider(_exchangeRateProvider);
     }
 
-    function setExchangeRateProvider(address _exchangeRateProvider) external onlyOwner {
-        exchangeRateProvider = IExchangeRateProvider(_exchangeRateProvider);
+    function setUsdtToken(address _usdtToken) external onlyOwner {
+        require(_usdtToken != address(0), "Invalid USDT address");
+        usdtToken = _usdtToken;
     }
 
     function setPrice(string memory price_id, uint256 amount) external onlyOwner {
@@ -62,58 +41,38 @@ contract ChrimaPayment {
         priceIdToAmount[price_id] = amount;
     }
 
-    function toggleContractDisabled() external onlyOwner {
-        contractDisabled = !contractDisabled;
+    function setProductRecipient(string memory product_id, address recipient) external onlyOwner {
+        require(recipient != address(0), "Invalid recipient");
+        productIdToRecipient[product_id] = recipient;
     }
 
     function processTransaction(
-        address token,
-        address recipient,
         string memory product_id,
         string memory price_id,
         string memory user_id
-    ) external payable whenEnabled {
-        require(recipient != address(0), "Invalid recipient");
+    ) external {
+        require(usdtToken != address(0), "USDT token not set");
 
-        string memory symbol = tokenSymbols[token];
-        require(bytes(symbol).length > 0, "Token symbol not set");
+        address recipient = productIdToRecipient[product_id];
+        require(recipient != address(0), "Recipient not set for product");
 
-        uint256 currencyAmount = priceIdToAmount[price_id];
-        require(currencyAmount > 0, "Price not set");
+        uint256 usdtAmount = priceIdToAmount[price_id];
+        require(usdtAmount > 0, "Price not set");
 
-        string memory currency = "usd";
-        (uint256 rate, ) = exchangeRateProvider.getRate(currency, symbol);
-        require(rate > 0, "Invalid rate");
+        IERC20 usdtContract = IERC20(usdtToken);
 
-        uint8 tokenDecimals = 18;
-        if (address(token) != address(0)) {
-            tokenDecimals = IERC20(token).decimals();
-        }
-
-        uint256 tokenAmount = (currencyAmount * rate) / 1e18;
-
-        if (tokenDecimals < 18) {
-            tokenAmount = tokenAmount / (10 ** (18 - tokenDecimals));
-        } else if (tokenDecimals > 18) {
-            tokenAmount = tokenAmount * (10 ** (tokenDecimals - 18));
-        }
-
-        if (address(token) == address(0)) {
-            require(msg.value == tokenAmount, "Incorrect ETH amount");
-        } else {
-            IERC20(token).transferFrom(msg.sender, recipient, tokenAmount);
-        }
+        require(
+            usdtContract.transferFrom(msg.sender, recipient, usdtAmount),
+            "USDT transfer failed"
+        );
 
         emit TransactionComplete(
-            product_id, 
-            price_id, 
-            user_id, 
-            msg.sender, 
-            recipient, 
-            token, 
-            tokenAmount
+            product_id,
+            price_id,
+            user_id,
+            msg.sender,
+            recipient,
+            usdtAmount
         );
     }
-
-    receive() external payable {}
 }
--- src/resources/contract/scripts/arguments.js
+++ src/resources/contract/scripts/arguments.js
@@ -1,1 +1,1 @@
-module.exports = [process.env.EXCHANGE_RATE_PROVIDER_CONTRACT_ADDRESS];
+module.exports = [];
