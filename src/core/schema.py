ba19a9f86779653edcfaa39279df5c56dd58e7f5
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-08-18 18:10:08 +0100

    refactor: Removed custom json encoders from CustomBaseModel opting for built-in json encoders

--- src/core/schema.py
+++ src/core/schema.py
@@ -1,15 +1,5 @@
-from datetime import datetime
-from enum import Enum
-from uuid import UUID
-
 from pydantic import BaseModel
 
 
 class CustomBaseModel(BaseModel):
-    model_config = {
-        "json_encoders": {
-            UUID: str,
-            datetime: lambda dt: dt.isoformat().replace("+00:00", "Z"), # For Zod compatibility
-            Enum: lambda e: e.value,
-        }
-    }
+    pass
